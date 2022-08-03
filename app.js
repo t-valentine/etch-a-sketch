889910009ddbc79e3313739439e07b33fbdfc2e6
Tobi V <hialmberi95@yahoo.com>
2022-08-03 14:30:05 -0500

    Take user input and validate

--- app.js
+++ app.js
@@ -2,18 +2,41 @@ const colorCell = (cell) => {
     cell.style.background = "red";
 }
 
-const createGrid = (input) => {
-    var width = (100/(Math.sqrt(input)))/100 * 495;
+const generateGrid = (input) => {
+    var width = (100/(Math.sqrt(input)))/100 * (500 - Math.sqrt(input));
+    console.log(width);
+    console.log(input);
     for (let i=0; i < input; i++){
         const cell = document.createElement('div')
         cell.classList.add('cell');
         cell.setAttribute("style",`width:${width}px`);
-        cell.onclick = function(){colorCell(cell)};
+        cell.onmouseover = function(){colorCell(cell)};
         //cell.addEventListener("click", colorCell(cell))
         const grids = document.getElementById('grid');
         grids.appendChild(cell);
     }
-};
+}
+addEventListener('load', (event) => {
+    const originalGrid = document.getElementById('cellQuantity').value
+    generateGrid(originalGrid * originalGrid);
+});
 
-window.addEventListener('load', createGrid(16));
+const createCanvas = () => {
+    document.getElementById('grid').innerHTML = '';
+    const quantity = document.getElementById('cellQuantity').value;
+    if (quantity <= 0) {
+        console.log("the size has to be bigger than 0!")
+        generateGrid(256);
+    }
+    else if (quantity > 100) {
+        console.log("the size has to be smaller than 100")
+        generateGrid(256);
+    }
+    else {
+        generateGrid(quantity * quantity);
+    };
+    
+}
+
+
 // onclick of Grid icon, change background-color to black
