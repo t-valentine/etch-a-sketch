0bcdd8e9fda91abbabb295639b64f137ef9c478d
Tobi V <hialmberi95@yahoo.com>
2022-08-03 13:54:27 -0500

    Add click to change color

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const colorCell = (cell) => {
+    cell.style.background = "red";
+}
+
 const createGrid = (input) => {
     var width = (100/(Math.sqrt(input)))/100 * 495;
     for (let i=0; i < input; i++){
         const cell = document.createElement('div')
         cell.classList.add('cell');
         cell.setAttribute("style",`width:${width}px`);
+        cell.onclick = function(){colorCell(cell)};
+        //cell.addEventListener("click", colorCell(cell))
         const grids = document.getElementById('grid');
         grids.appendChild(cell);
     }
