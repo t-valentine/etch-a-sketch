0def9e2bd01f1dfc09e1e86b7638cff57301f198
Tobi V <hialmberi95@yahoo.com>
2022-08-03 14:46:54 -0500

    Complete requirements

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const colorCell = (cell) => {
-    cell.style.background = "red";
+    cell.style.background = "#242423";
 }
 
 const generateGrid = (input) => {
-    var width = (100/(Math.sqrt(input)))/100 * (500 - Math.sqrt(input));
-    console.log(width);
-    console.log(input);
+    var width = (100/(Math.sqrt(input)))/100 * 500;
     for (let i=0; i < input; i++){
         const cell = document.createElement('div')
         cell.classList.add('cell');
@@ -21,20 +19,40 @@ addEventListener('load', (event) => {
     generateGrid(originalGrid * originalGrid);
 });
 
+const createError = (error) => {
+    const errordiv = document.getElementById('errors');
+    errordiv.innerHTML = '';
+    const p = document.createElement("p");
+    p.innerHTML = error;
+    errordiv.append(p);
+}
+
 const createCanvas = () => {
     document.getElementById('grid').innerHTML = '';
     const quantity = document.getElementById('cellQuantity').value;
-    if (quantity <= 0) {
-        console.log("the size has to be bigger than 0!")
-        generateGrid(256);
-    }
-    else if (quantity > 100) {
-        console.log("the size has to be smaller than 100")
-        generateGrid(256);
+    var regex = /[0-9]|\./;
+    if (regex.test(quantity)){
+        if (quantity <= 0) {
+            createError("the size has to be bigger than 0!");
+            console.log("the size has to be bigger than 0!");
+            generateGrid(256);
+        }
+        else if (quantity > 100) {
+            createError("the size has to be smaller than 100");
+            console.log("the size has to be smaller than 100");
+            generateGrid(256);
+        }
+        else {
+            createError("");
+            generateGrid(quantity * quantity);
+        };
     }
     else {
-        generateGrid(quantity * quantity);
-    };
+        createError('Please enter a numerical value');
+        console.log('Please enter a numerical value');
+        generateGrid(256);
+    }
+
     
 }
 
